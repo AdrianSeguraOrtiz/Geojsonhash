7a8690a325f5bd3c5c5ce53522d3324ccc42ed91
adrian_segura <adrianseor.99@uma.es>
2021-10-06 08:53:21 +0000

    add lint

--- geojsonhash/geojsonhash.py
+++ geojsonhash/geojsonhash.py
@@ -1,5 +1,7 @@
-import pygeohash as pgh
 from operator import itemgetter
+
+import pygeohash as pgh
+
 
 def get_geojson_id(geojson: dict) -> str:
     # TODO: Validate geojson
@@ -13,21 +15,21 @@ def get_geojson_id(geojson: dict) -> str:
         if geometry["type"] == "Polygon":
             points = geometry["coordinates"][0]
             points.pop()
-            first_point = min(points,key=itemgetter(0,1))
+            first_point = min(points, key=itemgetter(0, 1))
             representative_points.append(first_point)
             first_point_index = points.index(first_point)
             ordered_points = points[first_point_index:] + points[:first_point_index]
 
             points_ids = []
             for p in ordered_points:
-                point_id = pgh.encode(p[0], p[1], precision = 9)
+                point_id = pgh.encode(p[0], p[1], precision=9)
                 points_ids.append(point_id)
             polygon_id = "-".join(points_ids)
             polygon_ids.append(polygon_id)
 
-    new_order = [representative_points.index(x) for x in sorted(representative_points,key=itemgetter(0,1))]
+    new_order = [representative_points.index(x) for x in sorted(representative_points, key=itemgetter(0, 1))]
     ordered_polygon_ids = [polygon_ids[i] for i in new_order]
 
-    geojson_id="--".join(ordered_polygon_ids)
+    geojson_id = "--".join(ordered_polygon_ids)
 
     return geojson_id
